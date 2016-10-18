2eb71a33744853a634e58c381ae04d615fd8ea99
MichaelHazani <michael@michaelhazani.com>
2016-10-18 13:30:20 -0700

    add markov generator

--- src/js/TweetCard.js
+++ src/js/TweetCard.js
@@ -2,53 +2,74 @@ import React from "react";
 
 
 class TweetCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      border: " #ddd 3px solid"
+    constructor(props) {
+        super(props);
+        this.state = {
+            border: " #ddd 3px solid"
+        }
+        this._redOrGreen = this._redOrGreen.bind(this)
     }
-    this._redOrGreen = this._redOrGreen.bind(this)
-  }
 
 
-  _redOrGreen() {
-    console.log("redorgreen called")
-    if (this.props.content.isReal === true) {
-      this.setState({ border: "#2ECC40 3px solid"})
-    } else {
-      this.setState({ border: "#FF4136 3px solid"})
+    _redOrGreen() {
+        console.log("redorgreen called")
+        if (this.props.content.isReal === true) {
+            this.setState({
+                border: "#2ECC40 3px solid"
+            })
+        } else {
+            this.setState({
+                border: "#FF4136 3px solid"
+            })
+        }
     }
-  }
 
 
 
-  componentWillReceiveProps() {
-    this.setState({ border: "#ddd 3px solid" })
-  }
-
-
-  render() {
-    let cardStyles = {
-      backgroundColor: "white",
-      border: this.state.border
+    componentWillReceiveProps() {
+        this.setState({
+            border: "#ddd 3px solid"
+        })
     }
-    return (
-      <div>
-        <blockquote
-          className={"twitter-tweet"}
-          style={cardStyles}
-          onClick={this._redOrGreen}
-        >
-          <img src="https://pbs.twimg.com/profile_images/1980294624/DJT_Headshot_V2_bigger.jpg" />
-          <p>{this.props.content.text}</p>
-          <footer>
-              <cite>@realDonaldTrump</cite>
-          </footer>
-          <a href={this.props.url} target={"_blank"}>Link</a>
-        </blockquote>
-      </div>
-    )
-  }
+
+
+    render() {
+        let cardStyles = {
+            backgroundColor: "white",
+            border: this.state.border
+        }
+        return ( <
+            div >
+            <
+            blockquote className = {
+                "twitter-tweet"
+            }
+            style = {
+                cardStyles
+            }
+            onClick = {
+                this._redOrGreen
+            } >
+            <
+            img src = "https://pbs.twimg.com/profile_images/1980294624/DJT_Headshot_V2_bigger.jpg" / >
+            <
+            p > {
+                this.props.content.text
+            } < /p> <
+            footer >
+            <
+            cite > @realDonaldTrump < /cite> <
+            /footer> <
+            a href = {
+                this.props.url
+            }
+            target = {
+                "_blank"
+            } > Link < /a> <
+            /blockquote> <
+            /div>
+        )
+    }
 }
 
 export default TweetCard
--- src/js/app.js
+++ src/js/app.js
@@ -1,94 +1,125 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers } from "redux";
+import {
+    createStore,
+    combineReducers
+} from "redux";
 import deepFreeze from "deepfreeze";
-// import Counter from "./Counter.js";
-import expect, { createSpy, spyOn, isSpy } from "expect";
+import expect, {
+    createSpy,
+    spyOn,
+    isSpy
+} from "expect";
 import TweetCard from "./TweetCard"
+// import CreateFakeTweet from "./markovGenerator.js";
 import realTweetArray from "./data/realTweets";
 import fakeTweetArray from "./data/fakeTweets";
 
+
 const ranNum = (max) => {
-  return Math.floor(Math.random() * max)
+    return Math.floor(Math.random() * max)
 }
 
-const Button = ({ name, clickFunc }) => {
-  return (
-    <button onClick={clickFunc} >{name}</button>
-  )
+const Button = ({
+    name,
+    clickFunc
+}) => {
+    return ( <
+        button onClick = {
+            clickFunc
+        } > {
+            name
+        } < /button>
+    )
 }
 
 const goodSites = [
-  "http://www.naacp.org/",
-  "https://www.hillaryclinton.com/",
-  "http://maldef.org/immigration/litigation/",
-  "https://www.plannedparenthood.org/",
-  "https://www.icrc.org/eng/resources/documents/misc/57jqgr.htm"
+    "http://www.naacp.org/",
+    "https://www.hillaryclinton.com/",
+    "http://maldef.org/immigration/litigation/",
+    "https://www.plannedparenthood.org/",
+    "https://www.icrc.org/eng/resources/documents/misc/57jqgr.htm"
 ]
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      realTweetIndex: ranNum(3200),
-      fakeTweetIndex: ranNum(3200)
-    }
-    this._newTweet = this._newTweet.bind(this);
-    this._makeUrl = this._makeUrl.bind(this);
-  }
-
-  _newTweet() {
-    this.setState({
-      realTweetIndex: ranNum(3200),
-      fakeTweetIndex: ranNum(3200)
-    })
-  }
-
-
-
-  _makeUrl(tweetObject) {
-    if (tweetObject.hasOwnProperty("id_str")) {
-      console.log("makeurl called")
-      return "https://twitter.com/realDonaldTrump/status/" + tweetObject.id_str
-    } else {
-      const goodSite = Math.floor(Math.random() * goodSites.length)
-      console.log("goodSites index: " + goodSite)
-      return goodSites[goodSite]
+    constructor(props) {
+        super(props);
+        this.state = {
+            realTweetIndex: ranNum(3200),
+            fakeTweetIndex: ranNum(3200)
+        }
+        this._newTweet = this._newTweet.bind(this);
+        this._makeUrl = this._makeUrl.bind(this);
     }
 
-  }
-
-  render() {
-    console.log(realTweetArray[this.state.realTweetIndex])
-    console.log(fakeTweetArray[this.state.fakeTweetIndex])
-    const testStyle = {
-      backgroundColor: "green"
+    _newTweet() {
+        this.setState({
+            realTweetIndex: ranNum(3200),
+            fakeTweetIndex: ranNum(3200)
+        })
     }
 
-    return (
-      <div>
-        <Button
-          name={"Button here"}
-          clickFunc={this._newTweet}/>
-        <TweetCard
-          content={realTweetArray[this.state.realTweetIndex]}
-          url={this._makeUrl(realTweetArray[this.state.realTweetIndex])}
-          style={testStyle}
-        />
-        <TweetCard
-          content={fakeTweetArray[this.state.fakeTweetIndex]}
-          url={this._makeUrl(fakeTweetArray[this.state.fakeTweetIndex])}
-          style={testStyle}/>
-      </div>
-    )
-  }
+
+
+    _makeUrl(tweetObject) {
+        if (tweetObject.hasOwnProperty("id_str")) {
+            console.log("makeurl called")
+            return "https://twitter.com/realDonaldTrump/status/" + tweetObject.id_str
+        } else {
+            const goodSite = Math.floor(Math.random() * goodSites.length)
+            console.log("goodSites index: " + goodSite)
+            return goodSites[goodSite]
+        }
+
+    }
+
+    render() {
+        console.log(realTweetArray[this.state.realTweetIndex])
+        console.log(fakeTweetArray[this.state.fakeTweetIndex])
+        const testStyle = {
+            backgroundColor: "green"
+        }
+
+        return ( <
+            div >
+            <
+            Button name = {
+                "Button here"
+            }
+            clickFunc = {
+                this._newTweet
+            }
+            /> <
+            TweetCard content = {
+                realTweetArray[this.state.realTweetIndex]
+            }
+            url = {
+                this._makeUrl(realTweetArray[this.state.realTweetIndex])
+            }
+            style = {
+                testStyle
+            }
+            /> <
+            TweetCard content = {
+                this._generateFakeTweet.bind(this)
+            }
+            url = {
+                this._makeUrl(fakeTweetArray[this.state.fakeTweetIndex])
+            }
+            style = {
+                testStyle
+            }
+            /> < /
+            div >
+        )
+    }
 }
 
 const render = () => {
-  ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-  )
+    ReactDOM.render( <
+        App / > ,
+        document.getElementById("root")
+    )
 }
 
 render();
