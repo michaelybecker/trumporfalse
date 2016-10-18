0898101f52224fc14152cd7bebe065c66957ac9c
MichaelHazani <michael@michaelhazani.com>
2016-10-18 16:00:51 -0700

    finish initial markov integration

--- src/js/markovGenerator.js
+++ src/js/markovGenerator.js
@@ -1,8 +1,8 @@
 "use strict";
-import realTweetArray from "./data/realTweets";
+import corpus from "./data/fakeTweets";
 let MarkovChain = require('markovchain');
 //Only for text file generation
-// let fs = require('fs');
+let fs = require('fs');
 
 
 // ----------------------------------------------------------------------------------------
@@ -11,12 +11,19 @@ let MarkovChain = require('markovchain');
 // Generate the raw REAL tweets file. Only needs to happen once.
 // for (let i = 0; i < realTweetArray.length; i++) {
 //     const tweet = realTweetArray[i].text;
-//     fs.appendFile("realTweetsRaw.txt", tweet);
+//     fs.appendFile("realTweetsRaw.js", tweet);
 // }
 
-//Create a fake tweet.
+// fs.readFile("realTweetsRaw.js", 'utf8', (err, data) => {
+//   const formattedTweets = 'const corpus = "' + data.replace(/[^\x20-\x7E]/gmi, "").replace(/['"]+/g, '') + '"';
+//   // JSON.parse(formattedTweets);
+//   fs.writeFile("./data/fakeTweetsNew.js", formattedTweets);
+//   console.log("done!");
+// });
+
 
 const CreateFakeTweet = () => {
+    //Creates a fake tweet.
 
     const randFirstWord = () => {
         const firstWords = ["The", "I", "If", "thank", "you", "America"];
@@ -24,12 +31,13 @@ const CreateFakeTweet = () => {
     }
 
     const randSentenceLength = () => {
-        return Math.floor(Math.random() * 20) + 7;
+        return Math.floor(Math.random() * 30) + 15;
     }
 
-    let rawTweetsMarkov = new MarkovChain(fs.readFileSync('./data/realTweetsRaw.txt', 'utf8'));
+    let rawTweetsMarkov = new MarkovChain(corpus);
     const tweet = rawTweetsMarkov.start(randFirstWord()).end(randSentenceLength()).process();
-    console.log(tweet);
-}
+    // console.log(tweet);
 
+    return {text: tweet, isReal: false};
+}
 export default CreateFakeTweet;
