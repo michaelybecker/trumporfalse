712ae2d499d71e428a26eed10eaf4a5eab6335ee
llanginger <llanginger@gmail.com>
2016-10-31 20:05:13 -0700

    changed server port info

--- src/server/server.js
+++ src/server/server.js
@@ -10,8 +10,10 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"))
 });
 
-app.listen(3000, function(){
-  console.log("Example app listening on port 3000!");
+let port = Number(process.env.PORT || 3000)
+
+app.listen(port, function(){
+  console.log("Example app listening on port " + port);
 })
 
 app.get("/getTweet", function (req, res) {
